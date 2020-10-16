9f337d2d2858552ff5211221de49ece654317a7a
Wesley Ugang <35684516+wesuga@users.noreply.github.com>
2020-10-16 15:38:53 +0800

    Initial commit

---
+++ src/test/java/com/wesju/infoblog/InfoblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesju.infoblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
