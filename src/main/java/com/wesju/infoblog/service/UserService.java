e0510af17727c6d8b66fdea10f98de98429fb601
Wesley Ugang <35684516+wesuga@users.noreply.github.com>
2020-10-18 17:25:15 +0800

    Extend to UserDetailsService

--- src/main/java/com/wesju/infoblog/service/UserService.java
+++ src/main/java/com/wesju/infoblog/service/UserService.java
@@ -2,7 +2,8 @@ package com.wesju.infoblog.service;
 
 import com.wesju.infoblog.dto.UserRegisterDto;
 import com.wesju.infoblog.model.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
   User save(UserRegisterDto userRegisterDto);
 }
