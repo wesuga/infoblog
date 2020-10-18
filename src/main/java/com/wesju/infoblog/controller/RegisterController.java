f056a611c1a8434cf3feb84e815bfcb2187ebd33
Wesley Ugang <35684516+wesuga@users.noreply.github.com>
2020-10-18 13:32:21 +0800

    Store registered user account

--- src/main/java/com/wesju/infoblog/controller/RegisterController.java
+++ src/main/java/com/wesju/infoblog/controller/RegisterController.java
@@ -1,14 +1,32 @@
 package com.wesju.infoblog.controller;
 
+import com.wesju.infoblog.dto.UserRegisterDto;
+import com.wesju.infoblog.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/register")
 public class RegisterController {
+  private UserService userService;
+
+  public RegisterController(UserService userService) {
+    this.userService = userService;
+  }
+
   @GetMapping
-  public String register() {
+  public String register(Model model) {
+    model.addAttribute("user", new UserRegisterDto());
     return "register";
+  }
+
+  @PostMapping
+  public String registerUserAccount(@ModelAttribute("user") UserRegisterDto userRegisterDto) {
+    userService.save(userRegisterDto);
+    return "redirect:/login";
   }
 }
