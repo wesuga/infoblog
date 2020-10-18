c2e97e5782a01c435e9d21fb7b50a9ba29dde51b
Wesley Ugang <35684516+wesuga@users.noreply.github.com>
2020-10-18 17:51:33 +0800

    Set password encoder

--- src/main/java/com/wesju/infoblog/service/UserServiceImpl.java
+++ src/main/java/com/wesju/infoblog/service/UserServiceImpl.java
@@ -12,21 +12,24 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
-  private UserRepository userRepository;
+  private final UserRepository userRepository;
+  private final BCryptPasswordEncoder passwordEncoder;
 
   @Autowired
-  private UserServiceImpl(UserRepository userRepository) {
+  private UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
     this.userRepository = userRepository;
+    this.passwordEncoder = passwordEncoder;
   }
 
   @Override
   public User save(UserRegisterDto userRegisterDto) {
     User user = new User(userRegisterDto.getFirstName(), userRegisterDto.getLastName(),
-        userRegisterDto.getEmail(), userRegisterDto.getPassword(),
+        userRegisterDto.getEmail(), passwordEncoder.encode(userRegisterDto.getPassword()),
         Arrays.asList(new Role("ROLE_USER")));
 
     return userRepository.save(user);
