4f8c91c0ccb6c7692d9ab131a9e56dafee6c2845
Wesley Ugang <35684516+wesuga@users.noreply.github.com>
2020-10-19 14:25:04 +0800

    Fix wrong annotation

--- src/main/java/com/wesju/infoblog/config/SecurityConfig.java
+++ src/main/java/com/wesju/infoblog/config/SecurityConfig.java
@@ -6,13 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableWebMvc
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
   private UserService userService;
 
