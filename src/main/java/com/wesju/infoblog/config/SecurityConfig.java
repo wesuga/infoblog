47572cbd163a09e5fc1cb6979ea1a3f687c3f0a3
Wesley Ugang <35684516+wesuga@users.noreply.github.com>
2020-10-19 14:29:44 +0800

    Configure web security

--- src/main/java/com/wesju/infoblog/config/SecurityConfig.java
+++ src/main/java/com/wesju/infoblog/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,6 +19,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   public SecurityConfig(UserService userService) {
     this.userService = userService;
+  }
+
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    web.ignoring()
+        .antMatchers("/resources/**", "/static/**", "/webjars/**");
   }
 
   @Override
