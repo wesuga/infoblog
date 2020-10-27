fbf79d3bfb194b9c836c278e77410283585a389c
Wesley Ugang <35684516+wesuga@users.noreply.github.com>
2020-10-27 12:38:48 +0800

    Store user post

--- src/main/java/com/wesju/infoblog/controller/PostController.java
+++ src/main/java/com/wesju/infoblog/controller/PostController.java
@@ -1,17 +1,32 @@
 package com.wesju.infoblog.controller;
 
 import com.wesju.infoblog.model.Post;
+import com.wesju.infoblog.service.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/post")
 public class PostController {
+  private final PostService postService;
+
+  public PostController(PostService postService) {
+    this.postService = postService;
+  }
+
   @GetMapping
   public String showPostForm(Model model) {
     model.addAttribute("post", new Post());
     return "postform";
+  }
+
+  @PostMapping
+  public String createPost(@ModelAttribute("post") Post post) {
+    postService.save(post);
+    return "redirect:/";
   }
 }
