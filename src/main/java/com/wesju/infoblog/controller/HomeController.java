8408818017d1c2b7549b6f5325e75d469ae29267
Wesley Ugang <35684516+wesuga@users.noreply.github.com>
2020-10-30 18:04:46 +0800

    Show all posts

--- src/main/java/com/wesju/infoblog/controller/HomeController.java
+++ src/main/java/com/wesju/infoblog/controller/HomeController.java
@@ -1,17 +1,27 @@
 package com.wesju.infoblog.controller;
 
 import com.wesju.infoblog.model.Post;
+import com.wesju.infoblog.service.PostService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Collection;
+
 @Controller
 @RequestMapping("/")
 public class HomeController {
+  private final PostService postService;
+
+  public HomeController(PostService postService) {
+    this.postService = postService;
+  }
+
   @GetMapping
-  public String showPosts(Model model) {
-    model.addAttribute("post", new Post());
+  public String showAllPosts(Model model) {
+    Collection<Post> posts = postService.getAll();
+    model.addAttribute("posts", posts);
     return "home";
   }
 }
