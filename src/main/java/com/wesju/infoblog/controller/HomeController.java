556b4c05600acbe1fb153835a2e57840fab39e05
Wesley Ugang <35684516+wesuga@users.noreply.github.com>
2020-11-06 18:14:42 +0800

    Implement paging and sorting view controller

--- src/main/java/com/wesju/infoblog/controller/HomeController.java
+++ src/main/java/com/wesju/infoblog/controller/HomeController.java
@@ -2,12 +2,12 @@ package com.wesju.infoblog.controller;
 
 import com.wesju.infoblog.model.Post;
 import com.wesju.infoblog.service.PostService;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Collection;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -19,9 +19,12 @@ public class HomeController {
   }
 
   @GetMapping
-  public String showAllPosts(Model model) {
-    Collection<Post> posts = postService.getAll();
-    model.addAttribute("posts", posts);
+  public String showAllPosts(@RequestParam(defaultValue = "1") int page, Model model) {
+    Page<Post> pages = postService.findAllPaginated(page);
+
+    model.addAttribute("currentPage", page);
+    model.addAttribute("totalPages", pages.getTotalPages());
+    model.addAttribute("posts", pages.getContent());
     return "home";
   }
 }
