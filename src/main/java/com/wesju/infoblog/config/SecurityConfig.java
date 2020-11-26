823df851c820c0222f1d498c1f8a854101736692
Wesley Ugang <35684516+wesuga@users.noreply.github.com>
2020-11-26 22:57:54 +0800

    Edit URL permission

--- src/main/java/com/wesju/infoblog/config/SecurityConfig.java
+++ src/main/java/com/wesju/infoblog/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
-        .antMatchers("/register**", "/", "/login").permitAll()
+        .antMatchers("/register**", "/", "/login", "/post/**").permitAll()
         .anyRequest().authenticated()
         .and()
         .formLogin()
